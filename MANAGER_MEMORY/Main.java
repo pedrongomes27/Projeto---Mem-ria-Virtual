4d1c5b508655d08d01f1dbf4d856c67ef95a01e2
diogo162 <albanezdiogo@gmail.com>
2022-10-22 23:35:59 -0300

    remoção de método inútil

--- MANAGER_MEMORY/Main.java
+++ MANAGER_MEMORY/Main.java
@@ -93,17 +93,7 @@ public class Main {
         c.start();
     }
 
-    private static boolean isNumeric(String strNum) {
-        if (strNum == null) {
-            return false;
-        }
-        try {
-            double d = Double.parseDouble(strNum);
-        } catch (NumberFormatException nfe) {
-            return false;
-        }
-        return true;
-    }
+
 
 
 }
